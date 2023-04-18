244c009d2f0ddc2c10ce9cd92b6beea4e6a2cd4b
Paolo96700 <paolo.falcoapp@gmail.com>
2023-04-18 16:11:40 +0200

    Esercizio 2

---
+++ jsnack2/script.js
@@ -0,0 +1,10 @@
+let: Name;
+let myArr = [];
+
+Name = prompt("inserisci un nome o un cognome");
+
+const nomi = ["Andrea", "Vincenzo", "Gary", "Marco", "Antonio", "Mario"];
+const cognomi = ["Rosso", "Blu", "Verde", "Nero", "Giallo", "Viola"];
+
+
+
